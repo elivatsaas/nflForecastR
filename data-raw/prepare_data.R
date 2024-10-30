1faa8179d9cc2ece6a13dffe9c1e09633d154128
elivatsaas <157637354+elivatsaas@users.noreply.github.com>
2024-10-30 16:07:29 -0700

    Delete data-raw directory

--- data-raw/prepare_data.R
+++
@@ -1,5 +0,0 @@
-tidy_weeks <- prepare_weeks(2015:2023)
-tidy_games <- prepare_games(2015,2023, tidy_weeks)
-nfl_schedule_2024 <- read.csv(nfl-2024-UTC.csv)
-
-usethis::use_data(tidy_weeks, tidy_games, nfl_schedule_2024, overwrite = TRUE)
