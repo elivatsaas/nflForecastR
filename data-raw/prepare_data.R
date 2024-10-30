bedd5c36590f1f27d85827fc4c43ae2109a4b3e9
elivatsaas <157637354+elivatsaas@users.noreply.github.com>
2024-10-30 16:03:57 -0700

    Fixing Datasets

---
+++ data-raw/prepare_data.R
@@ -0,0 +1,9 @@
+tidy_weekly <- prepare_weekly(2015:2023)
+tidy_weekly <- na.omit(tidy_weekly)
+tidy_games <- prepare_games(2015,2023, tidy_weekly)
+tidy_games <- na.omit(tidy_games)
+nfl_schedule_2024 <- read.csv(file="data/nfl-2024-UTC.csv")
+
+usethis::use_data(tidy_weekly, overwrite = TRUE)
+usethis::use_data(tidy_games, overwrite = TRUE)
+usethis::use_data(nfl_schedule_2024, overwrite = TRUE)
