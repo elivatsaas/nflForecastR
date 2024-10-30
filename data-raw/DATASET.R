f33adeb720c6178e21274bc7ed4f0fecdd32bbca
elivatsaas <157637354+elivatsaas@users.noreply.github.com>
2024-10-30 15:44:07 -0700

    Add data and data-raw files

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,5 @@
-## code to prepare `DATASET` dataset goes here
+tidy_weeks <- prepare_weeks(2015:2023)
+tidy_games <- prepare_games(2015,2023, tidy_weeks)
+nfl_schedule_2024 <- read.csv(nfl-2024-UTC.csv)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(tidy_weeks, tidy_games, nfl_schedule_2024, overwrite = TRUE)
