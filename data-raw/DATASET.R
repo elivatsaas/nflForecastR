e7f05ad08041b92a00a123ef7d115dcf3c23e66a
elivatsaas <157637354+elivatsaas@users.noreply.github.com>
2024-10-30 15:48:32 -0700

    Delete data-raw directory

--- data-raw/DATASET.R
+++
@@ -1,4 +0,0 @@
-tidy_weekly <- prepare_games(2015:2023)
-tidy_games <- prepare_games(2015,2023,tidy_weekly)
-usethis::use_data(tidy_weekly, overwrite = TRUE)
-usethis::use_data(tidy_games, overwrite = TRUE)
