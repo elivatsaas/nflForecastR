2048209722302fed5e572683909657aa553aaac2
elivatsaas <157637354+elivatsaas@users.noreply.github.com>
2024-10-30 14:55:52 -0700

    Update DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,3 @@
-## code to prepare `DATASET` dataset goes here
-
+tidy_weekly <- prepare_games(2015:2023)
+tidy_games <- prepare_games(2015,2023,tidy_weekly)
 usethis::use_data(DATASET, overwrite = TRUE)
